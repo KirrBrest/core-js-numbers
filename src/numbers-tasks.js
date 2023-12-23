54f4fdace206a58d270dae86be2cb4c174c9abf7
KirrBrest <lukashchuk.kiryl@yandex.by>
2023-12-23 22:27:12 +0300

    feat: nazvanie_vashego_commita (Sat, Dec 23, 2023 10:27:12 PM)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -228,8 +228,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+
+function isPrime(n) {
+  // throw new Error('Not implemented');
+  let xx = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      xx = false;
+    }
+  }
+  return xx;
 }
 
 /**
